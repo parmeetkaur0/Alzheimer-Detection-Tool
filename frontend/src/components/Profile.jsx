ecf9dbb7f9a0f1a38f82a90e7ee41f6116960cea
Parmeet Kaur <kparmeet300@gmail.com>
2025-03-07 18:21:24 +0530

    commit changes

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -43,18 +43,17 @@ const Profile = () => {
 
   return (
     <div className="flex flex-col min-h-screen">
-      <Header />
       <Navbar />
       <main className="flex flex-col items-center justify-center flex-grow p-4">
         <h2 className="text-2xl mb-4 text-[#0E9272]">Profile</h2>
         <div className="w-full max-w-lg bg-white p-6 rounded shadow-md">
           <p><strong>Full Name:</strong> {user?.displayName}</p>
           <p><strong>Email:</strong> {user?.email}</p>
-          <p><strong>Gender:</strong> {profile.gender}</p>
+          {/* <p><strong>Gender:</strong> {profile.gender}</p>
           <p><strong>Blood Group:</strong> {profile.bloodGroup}</p>
           <p><strong>Age:</strong> {profile.age}</p>
           <p><strong>Address:</strong> {profile.address}</p>
-          <p><strong>Contact Number:</strong> {profile.contactNumber}</p>
+          <p><strong>Contact Number:</strong> {profile.contactNumber}</p> */}
         </div>
       </main>
       <Footer />
